375f16905d9fe064d71ac84b3db39a2924ce05c8
Heike Winkelvoß <public@egladil.de>
2019-10-27 08:45:49 +0100

    new release with quarkus-0.26.1

--- RELEASE-NOTES.md
+++ RELEASE-NOTES.md
@@ -1,4 +1,5 @@
 # mkadmin-server Release Notes
 
+__Release 0.2.0:__ upgrade to quarkus-0.26.1
 
 __Release 0.1.0:__ Infrastruktutest
--- src/main/java/de/egladil/web/mkadmin_server/filters/OriginReferrerFilter.java
+++ src/main/java/de/egladil/web/mkadmin_server/filters/OriginReferrerFilter.java
@@ -10,11 +10,10 @@ import java.util.List;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.NoContentException;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
 import org.apache.commons.lang3.StringUtils;
@@ -37,10 +36,7 @@ public class OriginReferrerFilter implements ContainerRequestFilter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(OriginReferrerFilter.class);
 
-	@Context
-	private HttpServletRequest servletRequest;
-
-	@ConfigProperty(name = "blockOnMissingOriginReferer", defaultValue = "false")
+	@ConfigProperty(name = "block.on.missing.origin.referer")
 	boolean blockOnMissingOriginReferer;
 
 	@ConfigProperty(name = "target.origin")
@@ -49,23 +45,23 @@ public class OriginReferrerFilter implements ContainerRequestFilter {
 	@Override
 	public void filter(final ContainerRequestContext requestContext) throws IOException {
 
-		final String pathInfo = servletRequest.getPathInfo();
+		UriInfo uriInfo = requestContext.getUriInfo();
+		String pathInfo = uriInfo.getPath();
 
-		if (NO_CONTENT_PATHS.contains(pathInfo) || "OPTIONS".equals(this.servletRequest.getMethod())) {
+		if (NO_CONTENT_PATHS.contains(pathInfo) || "OPTIONS".equals(requestContext.getMethod())) {
 
 			throw new NoContentException(pathInfo);
 		}
 
-		this.validateOriginAndRefererHeader();
-
+		validateOriginAndRefererHeader(requestContext);
 	}
 
-	private void validateOriginAndRefererHeader() throws IOException {
+	private void validateOriginAndRefererHeader(final ContainerRequestContext requestContext) throws IOException {
 
-		final String origin = servletRequest.getHeader("Origin");
-		final String referer = servletRequest.getHeader("Referer");
+		final String origin = requestContext.getHeaderString("Origin");
+		final String referer = requestContext.getHeaderString("Referer");
 
-		LOG.debug("Origin = [{}], Referer = [{]}", origin, referer);
+		LOG.debug("Origin = [{}], Referer=[{}]", origin, referer);
 
 		if (StringUtils.isBlank(origin) && StringUtils.isBlank(referer)) {
 
@@ -73,22 +69,22 @@ public class OriginReferrerFilter implements ContainerRequestFilter {
 
 			if (blockOnMissingOriginReferer) {
 
-				logErrorAndThrow(details);
+				logErrorAndThrow(details, requestContext);
 			}
 		}
 
 		if (!StringUtils.isBlank(origin)) {
 
-			checkHeaderTarget(origin);
+			checkHeaderTarget(origin, requestContext);
 		}
 
 		if (!StringUtils.isBlank(referer)) {
 
-			checkHeaderTarget(referer);
+			checkHeaderTarget(referer, requestContext);
 		}
 	}
 
-	private void checkHeaderTarget(final String headerValue) throws IOException {
+	private void checkHeaderTarget(final String headerValue, final ContainerRequestContext requestContext) throws IOException {
 
 		final String extractedValue = CommonHttpUtils.extractOrigin(headerValue);
 
@@ -97,17 +93,23 @@ public class OriginReferrerFilter implements ContainerRequestFilter {
 			return;
 		}
 
-		if (!targetOrigin.equals(extractedValue)) {
+		if (targetOrigin != null) {
 
-			final String details = "targetOrigin != extractedOrigin: [targetOrigin=" + targetOrigin + ", extractedOriginOrReferer="
-				+ extractedValue + "]";
-			logErrorAndThrow(details);
+			List<String> allowedOrigins = Arrays.asList(targetOrigin.split(" "));
+
+			if (!allowedOrigins.contains(extractedValue)) {
+
+				final String details = "targetOrigin != extractedOrigin: [targetOrigin=" + targetOrigin
+					+ ", extractedOriginOrReferer="
+					+ extractedValue + "]";
+				logErrorAndThrow(details, requestContext);
+			}
 		}
 	}
 
-	private void logErrorAndThrow(final String details) throws IOException {
+	private void logErrorAndThrow(final String details, final ContainerRequestContext requestContext) throws IOException {
 
-		final String dump = CommonHttpUtils.getRequesInfos(servletRequest);
+		final String dump = CommonHttpUtils.getRequestInfos(requestContext);
 		LOG.warn("Possible CSRF-Attack: {} - {}", details, dump);
 		throw new AuthException();
 	}
